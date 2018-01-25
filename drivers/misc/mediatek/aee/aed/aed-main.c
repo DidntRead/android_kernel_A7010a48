55494e4fb0094d5e190fb6d0bbbd978596157b4e
Shangbing Hu <shangbing.hu@mediatek.com>
2018-01-25 13:35:36 +0200

    [ALPS02530766] AEE: disable aee_kernel_dal_api.
    [Detail] the red screen feature should be available only for AEE,
    or it would make users confused about whether AEE workes well.
    
    [Solution] Disable this api.
    
    MTK-Commit-Id: 47ccf5e37ed8b58b51c90d29cb1f3fc644032af8
    
    Change-Id: Ia85f398e367e062c89bcb751a8887fdae44d55f3
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02530766
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/aed/aed-main.c
+++ drivers/misc/mediatek/aee/aed/aed-main.c
@@ -1803,7 +1803,7 @@ static void kernel_reportAPI(const AE_DEFECT_ATTR attr, const int db_opt, const 
 	}
 }
 
-#ifndef PARTIAL_BUILD
+#if 0
 void aee_kernel_dal_api(const char *file, const int line, const char *msg)
 {
 	LOGW("aee_kernel_dal_api : <%s:%d> %s ", file, line, msg);
@@ -1851,7 +1851,7 @@ void aee_kernel_dal_api(const char *file, const int line, const char *msg)
 #else
 void aee_kernel_dal_api(const char *file, const int line, const char *msg)
 {
-	LOGW("aee_kernel_dal_api : <%s:%d> %s ", file, line, msg);
+	LOGW("aee_kernel_dal_api has been phased out! caller info: <%s:%d> %s ", file, line, msg);
 }
 #endif
 EXPORT_SYMBOL(aee_kernel_dal_api);
